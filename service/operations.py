54eee3ecda1a4598b938ecfef9da4d02c3fd2996
Five Grant <5@fivegrant.com>
2023-07-31 09:44:23 -0500

    [broken?] start julia work

--- service/operations.py
+++ service/operations.py
@@ -5,6 +5,7 @@ import urllib
 import sys
 import logging
 import numpy as np
+import juliacall
 import requests
 from settings import settings
 from utils.rq_helpers import update_status_on_job_fail
@@ -26,6 +27,34 @@ TDS_URL = settings.TDS_URL
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
+
+
+@update_status_on_job_fail
+def simulate_sciml(request, *, job_id):
+    logging.debug(f"{job_id} (username - {request.username}): start simulate_sciml")
+
+    sim_results_url = TDS_URL + TDS_SIMULATIONS + job_id
+
+    update_tds_status(sim_results_url, status="running", start=True)
+
+    # Get model from TDS
+    amr_path = fetch_model(request.model_config_id, TDS_URL, TDS_CONFIGURATIONS, job_id)
+    
+    # Generate timepoints
+    time_count = request.timespan.end - request.timespan.start
+    timepoints=[step for step in range(1,time_count+1)]
+
+    output = load_and_sample_petri_model(
+        petri_model_or_path=amr_path, 
+        timepoints=timepoints, 
+        interventions=interventions,
+        visual_options=True, 
+        **request.extra.dict()
+    )
+    attach_files(output, TDS_URL, TDS_SIMULATIONS, job_id)
+    cleanup_job_dir(job_id)
+    logging.debug(f"{job_id} (username - {request.username}): finish simulate_sciml")
+
 
 @update_status_on_job_fail
 def simulate(request, *, job_id):
