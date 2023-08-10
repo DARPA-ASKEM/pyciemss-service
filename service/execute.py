a3a325bd0a45f700cd31a2982730ef589a0fefd6
Five Grant <5@fivegrant.com>
2023-08-10 09:01:28 -0500

    Comment Julia operations out

--- service/execute.py
+++ service/execute.py
@@ -1,5 +1,6 @@
 import logging
-from juliacall import newmodule
+
+# from juliacall import newmodule
 from settings import settings
 from utils.tds import (
     update_tds_status,
@@ -21,7 +22,7 @@ from pyciemss.Ensemble.interfaces import (  # noqa: F401
 TDS_SIMULATIONS = "/simulations/"
 TDS_URL = settings.TDS_URL
 
-jl = newmodule("SciMLIntegration")
+# jl = newmodule("SciMLIntegration")
 # jl.seval("using SciMLIntegration, PythonCall")
 
 logging.basicConfig()
@@ -33,16 +34,16 @@ def run(request, *, job_id):
     sim_results_url = TDS_URL + TDS_SIMULATIONS + job_id
     update_tds_status(sim_results_url, status="running", start=True)
 
-    if request.engine == "ciemss":
-        operation_name = request.__class__.pyciemss_lib_function
-        kwargs = request.gen_pyciemss_args(job_id)
-        if len(operation_name) == 0:
-            raise Exception("No operation provided in request")
-        else:
-            output = eval(operation_name)(**kwargs)
+    # if request.engine == "ciemss":
+    operation_name = request.__class__.pyciemss_lib_function
+    kwargs = request.gen_pyciemss_args(job_id)
+    if len(operation_name) == 0:
+        raise Exception("No operation provided in request")
     else:
-        operation = request.__class__.sciml_lib_function
-        output = operation(job_id, jl)
+        output = eval(operation_name)(**kwargs)
+    # else:
+    #     operation = request.__class__.sciml_lib_function
+    #     output = operation(job_id, jl)
 
     attach_files(output, TDS_URL, TDS_SIMULATIONS, job_id)
     cleanup_job_dir(job_id)
