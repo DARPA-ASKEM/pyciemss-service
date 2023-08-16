b0ad61d5bb73ab39a3fb773b6b1d206d79496da9
Five Grant <5@fivegrant.com>
2023-08-16 10:48:11 -0500

    Run through simulate e2e fully [caveats]

--- tests/integration/test_simulate.py
+++ tests/integration/test_simulate.py
@@ -1,4 +1,5 @@
-# import json
+import json
+import re
 
 import pytest
 
@@ -9,10 +10,9 @@ TDS_URL = settings.TDS_URL
 
 @pytest.mark.example_dir("simulate")
 def test_simulate_example(example_context, client, worker, requests_mock):
-    # job_id = example_context["tds_simulation"]["id"]
     request = example_context["request"]
-    # config_id = request["model_config_id"]
-    # model = json.loads(example_context["fetch"](config_id + ".json"))
+    config_id = request["model_config_id"]
+    model = json.loads(example_context["fetch"](config_id + ".json"))
 
     requests_mock.post(f"{TDS_URL}/simulations/", json={"id": None})
 
@@ -28,4 +28,24 @@ def test_simulate_example(example_context, client, worker, requests_mock):
     status = response.json()["status"]
     assert status == "queued"
 
+    tds_sim = example_context["tds_simulation"]
+    tds_sim["id"] = simulation_id
+
+    requests_mock.get(f"{TDS_URL}/simulations/{simulation_id}", json=tds_sim)
+    requests_mock.put(
+        f"{TDS_URL}/simulations/{simulation_id}", json={"status": "success"}
+    )
+    requests_mock.get(f"{TDS_URL}/model_configurations/{config_id}", json=model)
+    # TODO: Save files to locations where we can check against them
+    upload_url = re.compile("upload-url")
+    requests_mock.get(upload_url, json={"url": "http://WONTWORK"})
+    requests_mock.put("http://WONTWORK", json={"url": "WONTWORK"})
+
+    # TODO: Mock PyCIEMSS lib
     worker.work(burst=True)
+
+    response = client.get(
+        f"/status/{simulation_id}",
+    )
+    status = response.json()["status"]
+    assert status == "complete"
