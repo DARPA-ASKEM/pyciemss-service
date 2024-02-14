c75006096432aec4b139aaa3b259bd25702a1de8
Five Grant <5@fivegrant.com>
2024-02-14 14:57:06 -0600

    Use updated PyCIEMSS and flesh out optimize

--- service/models/operations/optimize.py
+++ service/models/operations/optimize.py
@@ -1,12 +1,40 @@
 from __future__ import annotations
 
-from typing import ClassVar, List, Optional
+from enum import Enum
+from typing import ClassVar, Dict, List, Optional
+
+import numpy as np
+import torch
 from pydantic import BaseModel, Field, Extra
 
 
 from models.base import OperationRequest, Timespan, InterventionObject
 from models.converters import convert_to_static_interventions
 from utils.tds import fetch_model, fetch_inferred_parameters
+
+
+class QOIMethod(Enum):
+    obs_nday_average = "obs_nday_average"
+
+
+def obs_nday_average_qoi(
+    samples: Dict[str, torch.Tensor], contexts: List, ndays: int = 7
+) -> np.ndarray:
+    """
+    Return estimate of last n-day average of each sample.
+    samples is is the output from a Pyro Predictive object.
+    samples[VARIABLE] is expected to have dimension (nreplicates, ntimepoints)
+    Note: last ndays timepoints is assumed to represent last n-days of simulation.
+
+    Taken from:
+    https://github.com/ciemss/pyciemss/blob/main/docs/source/interfaces.ipynb
+    """
+    dataQoI = samples[contexts[0]].detach().numpy()
+
+    return np.mean(dataQoI[:, -ndays:], axis=1)
+
+
+qoi_implementations = {QOIMethod.obs_nday_average: obs_nday_average_qoi}
 
 
 class OptimizeExtra(BaseModel):
@@ -28,6 +56,10 @@ class Optimize(OperationRequest):
         default_factory=list, example=[{"timestep": 1, "name": "beta", "value": 0.4}]
     )
     step_size: float = 1.0
+    qoi: QOIMethod
+    risk_bound: float
+    initial_guess_interventions: List[float]
+    bounds_interventions: List[List[float]]
     extra: OptimizeExtra = Field(
         None,
         description="optional extra system specific arguments for advanced use cases",
@@ -49,6 +81,10 @@ class Optimize(OperationRequest):
             "logging_step_size": self.step_size,
             "start_time": self.timespan.start,
             "end_time": self.timespan.end,
+            "qoi": qoi_implementations[self.qoi],
+            "risk_bound": self.risk_bound,
+            "initial_guess_interventions": self.initial_guess_interventions,
+            "bounds_interventions": self.bounds_interventions,
             "static_parameter_interventions": interventions,
             "inferred_parameters": inferred_parameters,
             **extra_options,
