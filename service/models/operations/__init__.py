0859b6bf0b1bcc4cea67562a3b14edba623e6f02
Five Grant <5@fivegrant.com>
2024-02-14 13:09:01 -0600

    Update PyCIEMSS to use patch

--- service/models/operations/__init__.py
+++ service/models/operations/__init__.py
@@ -1,3 +1,4 @@
 from models.operations.simulate import Simulate
 from models.operations.calibrate import Calibrate
 from models.operations.ensemble_simulate import EnsembleSimulate
+from models.operations.optimize import Optimize
