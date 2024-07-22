47072d4c2544f469167ccad2abd6c8b629e7dd8f
Tom Szendrey <T.Szendrey@hotmail.com>
2024-07-22 16:48:53 -0400

    lots of testing todo

--- service/models/converters.py
+++ service/models/converters.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 # TODO: Do not use Torch in PyCIEMSS Library interface
 import torch
 from utils.tds import fetch_interventions
-from typing import Dict
+from typing import Dict, Callable
 from models.base import HMIIntervention
 
 
@@ -24,6 +24,23 @@ def fetch_and_convert_static_interventions(policy_intervention_id, job_id):
     return convert_static_interventions(interventionList)
 
 
+def fetch_and_convert_dynamic_interventions(policy_intervention_id, job_id):
+    if not (policy_intervention_id):
+        return defaultdict(dict)
+    policy_intervention = fetch_interventions(policy_intervention_id, job_id)
+    interventionList: list[HMIIntervention] = []
+    for inter in policy_intervention["interventions"]:
+        intervention = HMIIntervention(
+            name=inter["name"],
+            applied_to=inter["applied_to"],
+            type=inter["type"],
+            static_interventions=inter["static_interventions"],
+            dynamic_interventions=inter["dynamic_interventions"],
+        )
+        interventionList.append(intervention)
+    return convert_dynamic_interventions(interventionList)
+
+
 # Used to convert from HMI Intervention Policy -> pyciemss static interventions.
 def convert_static_interventions(interventions: list[HMIIntervention]):
     if not (interventions):
@@ -36,6 +53,35 @@ def convert_static_interventions(interventions: list[HMIIntervention]):
             value = torch.tensor(float(static_inter.value))
             static_interventions[time][parameter_name] = value
     return static_interventions
+
+
+def make_var_threshold(var: str, threshold: torch.Tensor):
+    def var_threshold(time, state):
+        return state[var] - threshold
+
+    return var_threshold
+
+
+def convert_dynamic_interventions(interventions: list[HMIIntervention]):
+    if not (interventions):
+        return defaultdict(dict)
+    dynamic_parameter_interventions: Dict[
+        Callable[[torch.Tensor, Dict[str, torch.Tensor]], torch.Tensor],
+        Dict[str, any],
+    ] = defaultdict(dict)
+    for inter in interventions:
+        for dynamic_inter in inter.dynamic_interventions:
+            parameter_name = inter.applied_to
+            threshold_value = torch.tensor(float(dynamic_inter.threshold))
+            to_value = torch.tensor(float(dynamic_inter.value))
+            threshold_func = make_var_threshold(
+                dynamic_inter.parameter, threshold_value
+            )
+            dynamic_parameter_interventions[threshold_func].update(
+                {parameter_name: to_value}
+            )
+
+    return dynamic_parameter_interventions
 
 
 def convert_to_solution_mapping(config):
